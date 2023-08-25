33d277fb8d656ecad768d4013bb273222bcb5fd0
olaghattas <olaghattas@hotmail.com>
2023-08-25 19:23:32 -0400

    rm dir

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ola/smart_home/src/smart-home/external/particle_filter
-# Build directory: /home/ola/smart_home/src/smart-home/external/particle_filter/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ola/smart_home/src/smart-home/external/particle_filter/tests
-# Build directory: /home/ola/smart_home/src/smart-home/external/particle_filter/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
